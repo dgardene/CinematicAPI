a892efc4247941d3d5b3e99af4f358f7e449f31c
Deandre Gardener <dgardener02@gmail.com>
2023-07-01 16:47:25 -0500

    gradient fixes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // import React, { useState } from "react";
 import { useState, useEffect } from "react";
-// import "./App.css";
+import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com?apikey=25956992";
 // const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=25956992";
 
-const movie1 = {
-  Title: "Italian Spiderman",
-  Year: "2007",
-  imdbID: "tt2705436",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-};
+// const movie1 = {
+//   Title: "Italian Spiderman",
+//   Year: "2007",
+//   imdbID: "tt2705436",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+// };
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -26,11 +26,11 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovies("Spiderman");
+    searchMovies("Cars");
   }, []);
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1>CinematicApi</h1>
       <div className="search">
         <input
           placeholder="Search for Movies"
